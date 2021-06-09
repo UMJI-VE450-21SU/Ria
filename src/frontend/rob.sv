b4a8f7fd17a5158b421b2a2e3cdfad758fb60313
Tim Shi <timeshi@sjtu.edu.cn>
2021-06-09 15:15:00 +0800

    Update: `ROB` Discription

---
+++ src/frontend/rob.sv
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Project Name: RIA
+// Create Date: 2021/06/08
+// Contributor: Jian Shi
+// Reviewer: 
+// Module Name: rob
+// Target Devices: reorder buffer
+// Description: 
+// track the state of all inflight instructions in the pipeline
+// Dependencies: 
+// src/common/micro_op.svh, src/frontend/rat.sv
+//////////////////////////////////////////////////////////////////////////////////
+`include "../common/micro_op.svh"
+
+module rob (
+  input     clock
+);
+  
+endmodule
