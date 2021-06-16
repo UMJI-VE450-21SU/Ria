7a8ae1285a571f1b0a7616d1a8005dc3a5717909
Tim Shi <timeshi@sjtu.edu.cn>
2021-06-16 20:26:07 +0800

    Update: `I/O` for `ROB`

--- src/frontend/rob.sv
+++ src/frontend/rob.sv
@@ -13,7 +13,18 @@
 `include "../common/micro_op.svh"
 
 module rob (
-  input     clock
+  input           clock,
+  input           reset,
+
+  input           recover,
+  input   [31:0]  recover_pc,
+  input   micro_op_t  [`RENAME_WIDTH-1:0]   uop_retire,
+  input               [`RENAME_WIDTH-1:0]   retire_valid,
+  input   micro_op_t  [`RENAME_WIDTH-1:0]   uop_in,
+  input               [`RENAME_WIDTH-1:0]   in_valid,
+
+  output              [`RENAME_WIDTH-1:0]   retire_ready,
+  output              [`RENAME_WIDTH-1:0]   in_ready
 );
   
 endmodule
