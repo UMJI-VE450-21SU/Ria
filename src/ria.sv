1d554fc519be828953c1f34711b2f2390cd9aad2
Li Shi <shili2017@sjtu.edu.cn>
2021-06-22 16:26:10 +0800

    Initialize the top module (draft)

--- src/ria.sv
+++ src/ria.sv
@@ -1,5 +1,28 @@
 // Project: RISC-V SoC Microarchitecture Design & Optimization
 // Module:  Ria (Top Module)
-// Author:  Li Shi
+// Author:  Li Shi, Jian Shi, Yichao Yuan, Yiqiu Sun, Zhiyuan Liu
 // Date:    2021/06/21
 
+module ria (
+  input clock,
+  input reset,
+
+  // ======= icache related ==================
+  input        [31:0]  icache2core_data,
+  input                icache2core_data_valid,
+  output logic [31:0]  core2icache_addr,
+
+  
+);
+
+  inst_fetch inst_fetch (
+    .clock (clock),
+    .reset (reset),
+    .stall        (0),
+    .pc_predicted (0),
+    .branch_taken (0),
+    .branch_pc    (0),
+    .icache2core_data (icache2core_data)
+  );
+  
+endmodule
