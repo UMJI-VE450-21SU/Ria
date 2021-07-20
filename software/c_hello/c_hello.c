227e8e37c4144a8398429dceac11d99e7e8cfe3c
Li Shi <shili2048@gmail.com>
2021-07-20 23:23:54 +0800

    Run hello world

--- software/c_hello/c_hello.c
+++ software/c_hello/c_hello.c
@@ -1,17 +1,11 @@
-#define N 7
+#define N 14
 
-char msg[N] = "Hello\n";
+char msg[N] = "Hello World!\n";
 
 int main(void) {
   int i = 0;
-  // for (i = 0; i < N - 1; i++) {
-  //   asm volatile("sw %0, -8(x0)" :: "r"(msg[i]));
-  // }
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[0]));
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[1]));
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[2]));
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[3]));
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[4]));
-  asm volatile("sw %0, -8(x0)" :: "r"(msg[5]));
+  for (i = 0; i < N - 1; i++) {
+    asm volatile("sw %0, -8(x0)" :: "r"(msg[i]));
+  }
   return 0;
 }
